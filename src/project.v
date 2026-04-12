994565f39d4ca556e63b52a7bd8aacbb48659797
Chloe <chloezhong59@gmail.com>
2026-04-11 17:03:39 -0700

    working lorentz calculator + test bench

--- src/project.v
+++ src/project.v
@@ -16,8 +16,16 @@ module tt_um_bigturtle8_chip (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  reg [7:0] counter;
+
+  always @(posedge clk or negedge rst_n) begin
+      if (!rst_n)
+          counter <= 8'b0;
+      else
+          counter <= counter + 1;
+  end
+
+  assign uo_out  = counter;  
   assign uio_out = 0;
   assign uio_oe  = 0;
 
